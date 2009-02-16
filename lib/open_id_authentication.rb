johnl/open_id_authentication

commit 9235be22566039dfd0b333d2052c347f2f8946d2

Fix relative_url_root

lib/open_id_authentication.rb
@@ -222,7 +222,7 @@ module OpenIdAuthentication
       relative_url_root = self.class.respond_to?(:relative_url_root) ?
         self.class.relative_url_root.to_s :
         request.relative_url_root
-      "#{request.protocol + request.host_with_port + relative_url_root + request.path}"
+      "#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}#{request.path}"
     end
 
     def timeout_protection_from_identity_server

commit a0d3495f748b6f5f841c5b5369a04cfc0981c11e

Unfix relative_url_root, kinda reverting commit 9235be22566039dfd0b333d2052c347f2f8946d2 which broke support on Rails 2.1

lib/open_id_authentication.rb
@@ -224,7 +224,7 @@ module OpenIdAuthentication
       relative_url_root = self.class.respond_to?(:relative_url_root) ?
         self.class.relative_url_root.to_s :
         request.relative_url_root
-      "#{request.protocol}#{request.host_with_port}#{ActionController::Base.relative_url_root}#{request.path}"
+      "#{request.protocol}#{request.host_with_port}#{relative_url_root}#{request.path}"
     end
 
     def timeout_protection_from_identity_server